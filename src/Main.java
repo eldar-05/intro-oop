a2152d5ef386dda2bf5d0d51a1d1e2b89a49d990
eldar-05 <eldarzh123@gmail.com>
2024-02-08 12:28:39 +0600

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<Point> points = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            Point point = new Point();
+            point.x = scanner.nextInt();
+            point.y = scanner.nextInt();
+            point.z = Math.sqrt(point.x * point.x + point.y * point.y);
+            points.add(point);
+        }
+        Collection.sort(points);
+        Point furtherestPoint = points.get(n - 1);
+        System.out.println(furtherestPoint.x + " " + furtherestPoint.y);
+    }
+}
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class Point implements Comparable<Point> {
+    int x;
+    int y;
+    double z;
+    public int compareTo(Point other){
+        double a = this.z = other.z;
+        if(a < 0){
+            return -1;
+        }
+        else if(a > 0){
+            return 1;
+        }
+        else{
+            return 0;
         }
     }
 }
